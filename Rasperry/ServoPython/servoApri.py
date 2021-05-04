0bb02b411f7e13560064c767accd0065f9a2f461
Davide Bianchi <dadebianchi2003@gmail.com>
2021-05-04 17:03:19 +0200

    Creato il codice in python che permette di aprire e chiudere i servomotori collegati sul pin GPIO17

---
+++ Rasperry/ServoPython/servoApri.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # imposto la frequenza del pin a 50Hz
+p.start(0) # Inizializzo il pin
+
+try:
+
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
+  
