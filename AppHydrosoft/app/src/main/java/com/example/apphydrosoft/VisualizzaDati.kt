02a5a5e7c47eccee3c95439ffa9c308de77b5023
Emanuele Paci <paciemanuele.2@gmail.com>
2021-05-09 14:34:07 +0200

    Aggiunta nuova activity

--- AppHydrosoft/app/src/main/java/com/example/apphydrosoft/VisualizzaDati.kt
+++ AppHydrosoft/app/src/main/java/com/example/apphydrosoft/VisualizzaDati.kt
@@ -2,13 +2,11 @@ package com.example.apphydrosoft
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class VisualizzaDati : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btn1.findViewById(R.id.btnContinua);
-
-
+        setContentView(R.layout.activity_visualizza_dati)
     }
 }
