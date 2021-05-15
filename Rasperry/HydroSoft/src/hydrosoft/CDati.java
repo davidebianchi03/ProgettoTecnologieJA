095020d002c838c4db75e39edbfb7ac68297a9ca
Emanuele Paci <paciemanuele.2@gmail.com>
2021-05-15 14:48:08 +0200

    Modificata classe CDati, aggiunti metodi che restituiscono valore variabili e inserito rilevamento ora

--- Rasperry/HydroSoft/src/hydrosoft/CDati.java
+++ Rasperry/HydroSoft/src/hydrosoft/CDati.java
@@ -5,45 +5,69 @@ package hydrosoft;
  * @author pacie
  */
 public class CDati {
-    private int temperaturaAria;
-    private int umiditaAria;
+
+    private String oraRilevazione;
+    private String tipoPianta;
+    private float temperaturaAria;
+    private float umiditaAria;
     private boolean aperto; //true = aperto, false = chiuso
     private boolean bagnato; // true = bagnato, false = asciutto;
-    private String oraRilevazione;
 
-    public CDati(int temperaturaAria, int umiditaAria, boolean aperto, boolean bagnato, String oraRilevazione) {
+    public CDati(String tipoPianta, int temperaturaAria, int umiditaAria, boolean aperto, boolean bagnato) {
+        this.oraRilevazione = CFormatOra.getOra();
+        this.tipoPianta = tipoPianta;
         this.temperaturaAria = temperaturaAria;
         this.umiditaAria = umiditaAria;
         this.aperto = aperto;
         this.bagnato = bagnato;
-        this.oraRilevazione = oraRilevazione;
     }
 
-    public String getTemperaturaAria() {
+    public String getOraRilevazione() {
+        return this.oraRilevazione;
+    }
+
+    public String getTipoPianta() {
+        return this.tipoPianta;
+    }
+
+    public float getTemperaturaAria() {
+        return this.temperaturaAria;
+    }
+
+    public String getTemperaturaAriaAsString() {
         String temp = String.valueOf(temperaturaAria);
         return temp;
     }
 
-    public String getUmiditaAria() {
+    public float getUmiditaAria() {
+        return this.umiditaAria;
+    }
+
+    public String getUmiditaAriaAsString() {
         String temp = String.valueOf(umiditaAria);
         return temp;
     }
-
-    public String isAperto() {
+    
+    public boolean isAperto(){
+        return this.aperto;
+    }
+    
+    public String isApertoAsString() {
         String temp = String.valueOf(aperto);
         return temp;
     }
-
-    public String isBagnato() {
+    
+    public boolean isBagnato(){
+        return this.bagnato;
+    }
+    
+    public String isBagnatoAsString() {
         String temp = String.valueOf(bagnato);
         return temp;
     }
 
-    public String getOraRilevazione() {
-        return oraRilevazione;
-    }   
     @Override
-    public String toString(){
+    public String toString() {
         String temp = temperaturaAria + " " + umiditaAria + " " + aperto + " " + bagnato + " " + oraRilevazione;
         return temp;
     }
