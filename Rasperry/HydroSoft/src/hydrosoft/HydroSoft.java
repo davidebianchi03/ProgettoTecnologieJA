35222c07b2e13e568245044cf675455d821defa1
Davide Bianchi <dadebianchi2003@gmail.com>
2021-05-18 19:18:13 +0200

    Reso funzionante la parte di rasperry pi

--- Rasperry/HydroSoft/src/hydrosoft/HydroSoft.java
+++ Rasperry/HydroSoft/src/hydrosoft/HydroSoft.java
@@ -12,9 +12,13 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Davide, pacie, Edo
+ * @author Davide
  */
 
+/*
+    Il tutto sarebbe potuto essere fatto con i thread ma con essi non riesco a leggere il valore di umidità e temperatura (ho pensato che fosse un problema dovuto
+    alla condivisione dei pin GPIO tra i vari processi ma anche facendo la mutua esclusione non funzionano)
+ */
 public class HydroSoft {
 
     public static void main(String[] args) {
@@ -24,72 +28,112 @@ public class HydroSoft {
         GpioPinDigitalOutput pompa = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25);//pin pompa acqua
         CTipoPianta tipoPianta = new CTipoPianta();
         CJSONFile fileJSON = new CJSONFile();
-        
+
         float temperatura = 0.0f;
         float umidita = 0.0f;
         int umiditaTerreno = 0;
         boolean umido = true;
         boolean aperta = false;
-        int sogliaApertura = 20;//temperatura a cui la serra si apre
-        int sogliaChiusura = 18;//temperatura a cui la serra si chiude
+        int sogliaApertura = 19;//temperatura a cui la serra si apre
+        int sogliaChiusura = 17;//temperatura a cui la serra si chiude
         long lastUpload = System.currentTimeMillis();
-        
+
         CApriChiudiSerra.chiudiSerra();
-        
+
         String urlServer = "";
         String user = "";
         String pass = "";
-        
+
+        boolean pompaAttiva = false;
+
+        long oraAccensione = 0;
+        pompa.setState(PinState.LOW);//inizializzo la pompa a spenta
         while (true) {
-            if(pulsante.isHigh()){
-                while(pulsante.isHigh());
+            if (pulsante.isHigh()) {//se preme e rilascia il pulsante per cambiare il tipo di pianta
+                while (pulsante.isHigh());
                 tipoPianta.cambiaPianta();
             }
-            
+
             //rilevazione della temperatura dell'aria
             Float t = dht11.getTemperatura(7);
-            if(t != null)
+            if (t != null) {
                 temperatura = t.floatValue();
-            
-            
-            //rilevazione dell'umidità dell'aria
-            Float u = dht11.getUmidita(7);
-            if(u!= null)
-                umidita = u.floatValue();
-            
-            
+            }
+            //System.out.println("t"+temperatura);
             try {
-                //lettura umidita del terreno
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(HydroSoft.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //lettura umidita del terreno
+            try {
                 umiditaTerreno = CLeggiUmiditaTerreno.getUmiditaTerreno();
             } catch (IOException ex) {
                 Logger.getLogger(HydroSoft.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            
-            if(umiditaTerreno < tipoPianta.getSoglia()){//se la pianta è asciutta
+            if (umiditaTerreno < tipoPianta.getSoglia() || pompaAttiva == true) {//se la pianta è asciutta
+                //System.out.println("terreno secco");
                 umido = false;
                 //irrigo il terreno
-                pompa.setState(PinState.HIGH);
-                
-            }
-            else
+                if (System.currentTimeMillis() - oraAccensione > 120000 && !pompaAttiva) {//se sono passati almeno due minuti dall'ultima irrigazione
+                    //System.out.println("Pompa on");
+                    pompaAttiva = true;
+                    pompa.setState(PinState.HIGH);//accendo la pompa
+                    oraAccensione = System.currentTimeMillis();
+                }
+                //TODO:
+                //Impostare tempo spegnimento pompa -> 20s
+                if (pompaAttiva && System.currentTimeMillis() - oraAccensione > 20000) {
+                    //System.out.println("Pompa off");
+                    pompaAttiva = false;
+                    pompa.setState(PinState.LOW);//spengo la pompa
+                }
+
+            } else if (umiditaTerreno > tipoPianta.getSoglia()) {
                 umido = true;
-            
-            if(!aperta && temperatura > sogliaApertura){//se fa caldo apro la serra
+            }
+
+            //rilevazione dell'umidità dell'aria
+            Float u = dht11.getUmidita(7);
+            //System.out.println(u);
+            if (u != null) {
+
+                umidita = u.floatValue();
+            }
+            //System.out.println("u"+umidita);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(HydroSoft.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            if (!aperta && temperatura > sogliaApertura) {//se fa caldo apro la serra
                 aperta = true;
                 CApriChiudiSerra.apriSerra();
             }
-            
-            if(aperta && temperatura < sogliaChiusura){//se fa freddo chiudo la serra
+
+            if (aperta && temperatura < sogliaChiusura) {//se fa freddo chiudo la serra
                 aperta = false;
                 CApriChiudiSerra.chiudiSerra();
             }
-            
+            //System.out.println("oke");
             //aggiornamento file json su altervista tramite ftp ogni 2 minuti
-            if(System.currentTimeMillis() - lastUpload > 120000){
-                //TODO: Aggiornamento ftp
+            if (System.currentTimeMillis() - lastUpload > 120000) {
+                CDati dati = new CDati(tipoPianta.getPianta(), (int) temperatura, (int) umidita, aperta, umido);
+                fileJSON.readFile();
+                fileJSON.createJSONObject(dati);
+                fileJSON.appendJSONOject(dati);
+                fileJSON.createFile();
+                ConnessioneFTP ftp = new ConnessioneFTP(urlServer, user, pass);
+                String file = fileJSON.getNomeFileSR();//upload singola rilevazione
+                ftp.caricaFile(file);
+                file = fileJSON.getNomeFileLR();//upload più rilevazioni
+                ftp.caricaFile(file);
+                //System.out.println("FileUploaded");
+                lastUpload = System.currentTimeMillis();
             }
-            
 
         }
     }
