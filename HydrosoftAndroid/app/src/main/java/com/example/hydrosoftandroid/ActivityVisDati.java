ce8816245ba9f66664e33ff3f960a0cada5f18c7
Emanuele Paci <paciemanuele.2@gmail.com>
2021-05-19 07:32:57 +0200

    Creata activity per visualizzare i dati della serra

---
+++ HydrosoftAndroid/app/src/main/java/com/example/hydrosoftandroid/ActivityVisDati.java
@@ -0,0 +1,14 @@
+package com.example.hydrosoftandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityVisDati extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vis_dati);
+    }
+}
--- HydrosoftAndroid/app/src/main/java/com/example/hydrosoftandroid/MainActivity.java
+++ HydrosoftAndroid/app/src/main/java/com/example/hydrosoftandroid/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.hydrosoftandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnContinua = (Button) findViewById(R.id.btnContinua);
+        btnContinua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Benvenuto", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, ActivityVisDati.class);
+                startActivity(intent);
+            }
+        });
     }
 }
